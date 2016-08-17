625de15d3da4ffc5ca0bee27bc8ed701c5ab0aee
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-08-18 07:55:12 +1200

    initial commit

---
+++ src/test.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import express from 'express';
+
+const add = (a : number, b : number) : number => {
+  return a + b;
+}
+
+const app = express();
+
+app.get('/test', (req, res) => res.json(add(1,2)));
+
+app.listen(9000, () => console.log('server started!'));
