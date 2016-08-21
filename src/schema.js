fd4e729c3da1f0866dd9fdf01eb8e37af370bf53
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-08-21 12:36:22 +1200

    added person type and more flow checks

--- src/schema.js
+++ src/schema.js
@@ -9,23 +9,56 @@ import {
   GraphQLList,
   GraphQLNonNull,
   GraphQLSchema,
-  GraphQLString,
+  GraphQLString
 } from 'graphql';
+
+interface imdbData {
+  id: string,
+  type: string
+};
+
+interface imdbTitleData extends imdbData {
+  cast: [string]
+};
+
+interface imdbPersonData extends imdbData {
+  filmography: [FilmType]
+};
+
+type imdbType = { data: imdbData };
+type imdbTitleType = { data: imdbTitleData };
+type imdbPersonType = { data: imdbPersonData };
+
+type JSONObject = { [key:string]: mixed };
+
+const searchableProps = {
+  id: {
+    type: GraphQLString,
+    resolve: (root: imdbType): string => root.data.id
+  },
+  type: {
+    type: GraphQLString,
+    resolve: (root: imdbType): string => root.data.type
+  }
+};
 
 const getType = (id: string) : Promise<string> => rp(`http://imdb.wemakesites.net/api/${id}`).then(res => res);
 
-const resolveType = (root: string) : GraphQLObjectType => {
-  const type = JSON.parse(root).data.type;
+const resolveType = (root: imdbType) : GraphQLObjectType => {
+  const type = root.data.type;
   switch(type) {
     case 'title':
     return TitleType;
+    case 'name':
+    return PersonType;
   }
 };
 
 const SearchableType = new GraphQLInterfaceType({
   name: 'Searchable',
   fields: {
-    searchById: { type: GraphQLString }
+    id: { type: GraphQLString },
+    type: { type: GraphQLString }
   },
   resolveType: resolveType
 });
@@ -34,10 +67,7 @@ const TitleType = new GraphQLObjectType({
   name: 'Title',
   interfaces: [SearchableType],
   fields: {
-    id: {
-      type: GraphQLString,
-      resolve: (root: string): string => JSON.parse(root).data.id
-    },
+    ...searchableProps,
     cast: {
       type: new GraphQLList(GraphQLString),
       args: {
@@ -45,19 +75,45 @@ const TitleType = new GraphQLObjectType({
           type: GraphQLInt
         }
       },
-      resolve: (root: string, { first }: any): [string] => {
-        const cast = JSON.parse(root).data.cast;
+      resolve: (root: imdbTitleType, { first }: any): [string] => {
+        const cast = root.data.cast;
         return cast.slice(0, first || cast.length);
       }
-    },
-    searchById: {
-      type: GraphQLString
+    }
+  },
+});
+
+const FilmType = new GraphQLObjectType({
+  name: 'Film',
+  fields: {
+    title: { type: GraphQLString },
+    year: { type: GraphQLString },
+    info: { type: GraphQLString }
+  }
+});
+
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  interfaces: [SearchableType],
+  fields: {
+    ...searchableProps,
+    filmography: {
+      type: new GraphQLList(FilmType),
+      args: {
+        first: {
+          type: GraphQLInt
+        }
+      },
+      resolve: (root: imdbPersonType, { first }: any): [FilmType] => {
+        const filmography = root.data.filmography;
+        return filmography.slice(0, first || filmography.length);
+      }
     }
   },
 });
 
 const schema = new GraphQLSchema({
-  types: [TitleType, SearchableType],
+  types: [TitleType, PersonType, SearchableType],
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -68,7 +124,7 @@ const schema = new GraphQLSchema({
             type: new GraphQLNonNull(GraphQLString)
           }
         },
-        resolve: (root, { id }): Promise<string> => getType(id).then(res => res)
+        resolve: (root, { id }): Promise<JSONObject> => getType(id).then(JSON.parse)
       }
     }
   })
