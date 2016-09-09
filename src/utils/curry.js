8c2d5bad890086c057817bf60c99766aca905c0c
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-10 08:50:38 +1200

    updated trace signature

--- src/utils/curry.js
+++ src/utils/curry.js
@@ -3,6 +3,6 @@
 import rp from 'request-promise';
 import { tap, curry } from 'ramda';
 
-export const trace      = (msg: any): any => tap(x => console.log(msg, x));
+export const trace      = (msg: string): * => tap(x => console.log(msg, x));
 export const request    = curry((opts: JSONObject): Promise<JSONObject> => rp(opts));
 export const promiseAll = curry((arr: [Promise<any>]): Promise<any> => Promise.all(arr));
