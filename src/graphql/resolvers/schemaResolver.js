99c2e9329e544530a6f77559a7faf237c3a85185
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-10 09:09:56 +1200

    beginning of searchResolver refactor

--- src/graphql/resolvers/schemaResolver.js
+++ src/graphql/resolvers/schemaResolver.js
@@ -2,7 +2,11 @@
 
 import { searchById } from '../../services/imdbService';
 
-import { compose, trim } from 'ramda';
+import { compose, trim, ifElse, take, equals } from 'ramda';
+
+const isTitle = (id: string): bool => compose(equals('tt'), take(2))(id);
+const ifTitle = ifElse(isTitle);
+//const searchTitle = (id: string): Promise<JSONObject> => id;
 
 //TODO: create searchPerson and searchTitle
-export const search = (root: any, { id }: any): Promise<JSONObject> => compose(searchById, trim)(id);
+export const search = (root: any, { id }: any): Promise<JSONObject> => compose(ifTitle(searchById, searchById), trim)(id);
