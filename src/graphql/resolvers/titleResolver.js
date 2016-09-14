d92347305331ed3e8dfdfb12356a285133555789
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-14 14:49:36 +1200

    cleanup

--- src/graphql/resolvers/titleResolver.js
+++ src/graphql/resolvers/titleResolver.js
@@ -8,20 +8,19 @@ import { map, prop, path, head, compose, split,
   /* $FlowIgnore: 'composeP' not included in declaration */
   composeP } from 'ramda';
 
-const strToArr        = split(',');
-const filmCast        = (data: ImdbTermResultData): ?[string] => path(['results', 'names'])(data);
-const mapIds          = (arr: [{ [key:string]: string }]): [string] => map(prop('id'))(arr);
+const getAllMatches    = (data: ImdbTermResultData): ?[string] => path(['results', 'names'])(data);
+const getExactMatch    = (arr: [{ [key:string]: string }]): [string] => map(prop('id'))(arr);
 /* $FlowIgnore: there seems to be a bug in the declaration of the intersection - code works fine */
-const getFirstCastId  = (imdbResult: ImdbTermResultData): string => compose(head, mapIds, filmCast)(imdbResult);
+const getExactMatchId  = (imdbResult: ImdbTermResultData): string => compose(head, getExactMatch, getAllMatches)(imdbResult);
 
 export const rated      = ({ Rated }: ImdbMergedTitleData): string => Rated;
 export const language   = ({ Language }: ImdbMergedTitleData): string => Language;
 export const votes      = ({ imdbVotes }: ImdbMergedTitleData): string => imdbVotes;
 export const metascore  = ({ Metascore }: ImdbMergedTitleData): string => Metascore;
 export const rating     = ({ imdbRating }: ImdbMergedTitleData): string => imdbRating;
-export const director   = ({ Director }: ImdbMergedTitleData): [string] => strToArr(Director);
+export const director   = ({ Director }: ImdbMergedTitleData): [string] => split(',', Director);
 export const released   = ({ released }: ImdbMergedTitleData, { format }: any): string => formatDate(released, format);
 export const cast       = ({ cast }: ImdbMergedTitleData, { first }: any): Promise<[ImdbPersonData]> => {
   const toSearch: [string] = firstN(first, cast);
-  return composeP(map(searchById), map(getFirstCastId), searchTerms)(toSearch);
+  return composeP(map(searchById), map(getExactMatchId), searchTerms)(toSearch);
 };
