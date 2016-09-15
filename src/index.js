2ab0fdea02cef8f675a1b56d7d2b26c2c8fdfd06
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-15 18:06:08 +1200

    initial offline solution - needs more work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 /* @flow */
 
+import schema from './graphql/schema';
+
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './graphql/schema';
+import { when, equals } from 'ramda';
+
+//Very simple and early version that works fine
+//TODO: Store more data locally
+//TODO: Dynamically load data into cache
+//TODO: Move to another file ?? 
+const hydrateCache = (): bool => {
+  const cache = require('./utils/cache');
+  const title = require('./mocks/title');
+  cache.set(title.id)(title);
+  return true;
+};
+
+when(equals('true'), hydrateCache)(process.env.offline);
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
