be0873f4cb68c80b231aa9a2456fab4983e852fc
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-28 20:21:27 +1300

    Title now only resolves with Omdb

--- src/graphql/resolvers/schemaResolver.js
+++ src/graphql/resolvers/schemaResolver.js
@@ -1,17 +1,16 @@
 /* @flow */
 
 import * as cache from '../../utils/cache';
-import { promiseAll } from '../../utils/curry';
 import { searchById, searchOmdb } from '../../services/imdbService';
 
-import { compose, trim, ifElse, take, equals, mergeAll,
+import { compose, trim, ifElse, take, equals,
   /* $FlowIgnore: 'composeP' not included in declaration */
   composeP } from 'ramda';
 
 const isTitle         = (id: string): bool => compose(equals('tt'), take(2))(id);
 const searchPerson    = (id: string): Promise<ImdbPersonData> => composeP(cache.set(id), searchById)(id);
-const searchTitle     = (id: string): Promise<ImdbMergedTitleData> => composeP(cache.set(id), mergeAll, promiseAll)([searchById(id), searchOmdb(id)]);
-const searchResource  = (id: string): Promise<ImdbMergedTitleData|ImdbPersonData> => ifElse(isTitle, searchTitle, searchPerson)(id);
-const fromCacheOrApi  = (id: string): Promise<ImdbMergedTitleData|ImdbPersonData> => ifElse(cache.has, cache.get, searchResource)(id);
+const searchTitle     = (id: string): Promise<OmdbTitleResultData> => composeP(cache.set(id), searchOmdb)(id);
+const searchResource  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(isTitle, searchTitle, searchPerson)(id);
+const fromCacheOrApi  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(cache.has, cache.get, searchResource)(id);
 
-export const search   = (root: any, { id }: any): Promise<ImdbMergedTitleData|ImdbPersonData> => compose(fromCacheOrApi, trim)(id);
+export const search   = (root: any, { id }: any): Promise<OmdbTitleResultData|ImdbPersonData> => compose(fromCacheOrApi, trim)(id);
