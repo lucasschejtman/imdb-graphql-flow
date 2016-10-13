70a2185a1e1f88f66aa11d0e5d021763644dfae2
Lucas <lucas.schejtman@gmail.com>
2016-10-13 13:41:52 +1300

    added cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 
 import schema from './graphql/schema';
 
+import cors from 'cors';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 
 const app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
