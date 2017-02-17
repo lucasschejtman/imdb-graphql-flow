1cd6bdcc6a70b1e145ebe18a2473b81169a8c44e
Lucas <lucas.schejtman@gmail.com>
2017-02-17 14:02:24 +1300

    clean up

--- src/graphql/resolvers/schemaResolver.js
+++ src/graphql/resolvers/schemaResolver.js
@@ -7,8 +7,8 @@ import { compose, trim, ifElse, take, equals } from 'ramda';
 
 const isTitle         = (id: string): bool => compose(equals('tt'), take(2))(id);
 // Unfortunately the GraphQL driver expect a promise
-const searchPerson    = (id: string): Promise<OmdbTitleResultData> => searchById(id).promise().then(cache.set(id));
-const searchTitle     = (id: string): Promise<OmdbTitleResultData> => searchOmdb(id).promise().then(cache.set(id));
+const searchPerson    = (id: string): Promise<OmdbTitleResultData> => searchById(id).promise().then(cache.set(id)).catch(console.log);
+const searchTitle     = (id: string): Promise<OmdbTitleResultData> => searchOmdb(id).promise().then(cache.set(id)).catch(console.log);
 const searchResource  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(isTitle, searchTitle, searchPerson)(id);
 const fromCacheOrApi  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(cache.has, cache.get, searchResource)(id);
 
--- src/utils/curry.js
+++ src/utils/curry.js
@@ -7,6 +7,5 @@ import Future from 'fluture';
 export const trace      = (msg: string): * => tap(x => console.log(msg, x));
 export const fetch      = (opts: JSONObject): Future<any> => Future((rej, res) => void request(opts, (err, response, body) => {
     if(err) return rej(err);
-    console.log(body);
     return res(body);
 }));
