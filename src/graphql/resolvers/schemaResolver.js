f42cf3fc63a97b2ab46a6fe74bdad315522e3d0e
Lucas <lucas.schejtman@gmail.com>
2017-02-21 14:31:36 +1300

    added error for invalid mutations

--- src/graphql/resolvers/schemaResolver.js
+++ src/graphql/resolvers/schemaResolver.js
@@ -1,11 +1,11 @@
 /* @flow */
 
-import { trace } from '../../utils/curry';
+import { trace, gqlError } from '../../utils/curry';
 import * as cache from '../../utils/cache';
 import { startsWith } from '../../utils/string';
 import { searchById, searchOmdb } from '../../services/imdbService';
 
-import { compose, trim, ifElse, when, assoc } from 'ramda';
+import { compose, trim, ifElse, always, assoc } from 'ramda';
 
 const isTitle         = (id: string): bool => startsWith('tt')(id);
 // Unfortunately the GraphQL driver expect a promise
@@ -17,4 +17,7 @@ const fromCacheOrApi  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData
 export const search   = (root: any, { id }: any): Promise<OmdbTitleResultData|ImdbPersonData> => compose(fromCacheOrApi, trim)(id);
 
 // test save
-export const updateTitle = (root: any, { id, title }: any) => when(cache.has, compose(cache.set(id), assoc('Title', title), cache.get))(id);
+export const updateTitle = (root: any, { id, title }: any) => {
+    const update = compose(cache.set(id), assoc('Title', title), cache.get);
+    return ifElse(cache.has, update, gqlError(`Title with id ${id} does not exists`))(id);
+};
--- src/utils/curry.js
+++ src/utils/curry.js
@@ -1,10 +1,11 @@
 /* @flow */
 
 import request from 'request';
-import { tap } from 'ramda';
+import { tap, always } from 'ramda';
 import Future from 'fluture';
 
 export const trace      = (msg: string): * => tap(x => console.log(msg, x));
+export const gqlError   = (err: string): () => Promise<Error> => always(Promise.reject(new Error(err)));
 export const fetch      = (opts: JSONObject): Future<any> => Future((rej, res) => void request(opts, (err, response, body) => {
     if(err) return rej(err);
     return res(body);
