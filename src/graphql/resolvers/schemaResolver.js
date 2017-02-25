063c4694c6ccd2641f553a53cad09764353d1f44
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-02-25 17:40:59 +1300

    back to R

--- src/graphql/resolvers/schemaResolver.js
+++ src/graphql/resolvers/schemaResolver.js
@@ -5,25 +5,25 @@ import * as cache from '../../utils/cache';
 import { startsWith } from '../../utils/string';
 import { searchById, searchOmdb } from '../../services/imdbService';
 
-import { compose, trim, ifElse, assoc, converge, prop, identity, uncurryN } from 'ramda';
+import R from 'ramda';
 
 const isTitle         = (id: string): bool => startsWith('tt')(id);
 // Unfortunately the GraphQL driver expect a promise
 const searchPerson    = (id: string): Promise<OmdbTitleResultData> => searchById(id).promise().then(cache.set(id)).catch(console.log);
 const searchTitle     = (id: string): Promise<OmdbTitleResultData> => searchOmdb(id).promise().then(cache.set(id)).catch(console.log);
-const searchResource  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(isTitle, searchTitle, searchPerson)(id);
-const fromCacheOrApi  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => ifElse(cache.has, cache.get, searchResource)(id);
+const searchResource  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => R.ifElse(isTitle, searchTitle, searchPerson)(id);
+const fromCacheOrApi  = (id: string): Promise<OmdbTitleResultData|ImdbPersonData> => R.ifElse(cache.has, cache.get, searchResource)(id);
 
-export const search   = (root: any, { id }: any): Promise<OmdbTitleResultData|ImdbPersonData> => compose(fromCacheOrApi, trim)(id);
+export const search   = (root: any, { id }: any): Promise<OmdbTitleResultData|ImdbPersonData> => R.compose(fromCacheOrApi, R.trim)(id);
 
 // test update
 export const updateTitle = (root: any, { id, title }: any) => {
-    const update = compose(cache.set(id), assoc('Title', title), cache.get);
-    return ifElse(cache.has, update, gqlError(`Title with id ${id} does not exists`))(id);
+    const update = R.compose(cache.set(id), R.assoc('Title', title), cache.get);
+    return R.ifElse(cache.has, update, gqlError(`Title with id ${id} does not exists`))(id);
 };
 
-export const createTitle = (root:any, args: { [key:string]: any }) => compose(
-    converge(uncurryN(2, cache.set), [prop('imdbID'), identity]),
-    assoc('Type', 'movie'),
-    prop('title')
+export const createTitle = (root:any, args: { [key:string]: any }) => R.compose(
+    R.converge(R.uncurryN(2, cache.set), [R.prop('imdbID'), R.identity]),
+    R.assoc('Type', 'movie'),
+    R.prop('title')
 )(args);
