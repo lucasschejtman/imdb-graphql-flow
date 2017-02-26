bbee100c7bf7e9a67d9692cce8fd9b2090f9c669
Lucas <lucas.schejtman@gmail.com>
2017-02-27 10:49:05 +1300

    safe guarding resolver

--- src/graphql/resolvers/titleResolver.js
+++ src/graphql/resolvers/titleResolver.js
@@ -2,6 +2,7 @@
 
 import { formatDate } from '../../utils/date';
 
+import { fromNullable } from 'data.maybe';
 import { map, compose, split, trim, propEq } from 'ramda';
 
 const omdbDateFormat    = "DD MMM YYYY";
@@ -9,8 +10,8 @@ const strToArray        = (str: string): [string] => compose(map(trim), split(',
 
 export const Type       = propEq('Type');
 export const Title      = ({ Title }: OmdbTitleResultData): string => Title;
-export const Genre      = ({ Genre }: OmdbTitleResultData): [string] => strToArray(Genre);
-export const Writer     = ({ Writer }: OmdbTitleResultData): [string] => strToArray(Writer);
-export const Director   = ({ Director }: OmdbTitleResultData): [string] => strToArray(Director);
-export const Actors     = ({ Actors }: OmdbTitleResultData): [string] => strToArray(Actors);
+export const Genre      = ({ Genre }: OmdbTitleResultData): [string] => fromNullable(Genre).map(strToArray).getOrElse([]);
+export const Writer     = ({ Writer }: OmdbTitleResultData): [string] => fromNullable(Writer).map(strToArray).getOrElse([]);
+export const Director   = ({ Director }: OmdbTitleResultData): [string] => fromNullable(Director).map(strToArray).getOrElse([]);
+export const Actors     = ({ Actors }: OmdbTitleResultData): [string] => fromNullable(Actors).map(strToArray).getOrElse([]);
 export const Released   = ({ Released }: OmdbTitleResultData, { format }: { [key:string]:string }): string => formatDate(Released, omdbDateFormat, format || omdbDateFormat);
